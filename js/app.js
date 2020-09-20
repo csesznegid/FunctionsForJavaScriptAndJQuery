08132a64c38a449b8741f71d5a11e8b6c5abc556
Denes Csesznegi <denes.csesznegi@codecool.com>
2020-09-20 18:07:48 +0200

    feat: override jquery's val() method

---
+++ js/app.js
@@ -0,0 +1,32 @@
+/**
+ * The original jQuery val() method
+ *
+ * @type {function}
+ * @public
+ */
+let jQueryOriginalVal = $.fn.val;
+
+/**
+ * Define variables and functions
+ * when the whole document is loaded/ready
+ */
+$(document).ready(function() {
+    /**
+     * Override jQuery's val() method
+     *
+     * @param   {*} value
+     * @returns {string|*}
+     * @public
+     */
+    $.fn.val = function(value) {
+        // If no value was given and the field has the type "password"
+        // it gives back ":P" as the value
+        if (undefined === value && 'password' === $(this).prop('type')) {
+            return ':P';
+        }
+
+        // Invoke the original jQuery val() method
+        // with the current method's arguments as an Array
+        return jQueryOriginalVal.apply(this, Array.from(arguments));
+    };
+});
